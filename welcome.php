5d634161e78d043d27528130ea47649985575a86
faizalk4680 <112687817+faizalk4680@users.noreply.github.com>
2022-09-02 13:18:40 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -10,7 +10,7 @@
    <body>
       <h1>Welcome <?php echo $login_session; ?></h1> 
       <h2><a href = "logout.php">Sign Out</a></h2>
-      <h3><a href = "logout.php">Sign Out</a></h2>
+      <h3><a href = "upload.php">Upload Photo</a></h3>
    </body>
 
 </html>
